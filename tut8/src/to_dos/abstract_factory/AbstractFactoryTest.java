5e84ab64bb04b7494e5f70de40f2c1943313f446
Nguyễn Thúy <94062325+thuynguyenbm@users.noreply.github.com>
2022-06-12 23:43:42 +0700

    Update AbstractFactoryTest.java
    fix bugs

--- tut8/src/to_dos/abstract_factory/AbstractFactoryTest.java
+++ tut8/src/to_dos/abstract_factory/AbstractFactoryTest.java
@@ -1,32 +1,33 @@
-package to_dos.abstract_factory;
+package complete.abstract_factory;
 
-/* Create the AbstractFactoryTest class which uses the FactoryProducer
- * to get AbstractFactory in order to get factories
+/* Create the AbstractFactoryTest class which uses the FactoryProducer 
+ * to get AbstractFactory in order to get factories 
  * of concrete classes by passing an information such as type.
  */
 
 public class AbstractFactoryTest {
-    //TO-DO: Implement the main() method for testing purpose
-    public static void main(String[] args) {
-        // get shape factory
-        ShapeFactory factory = new ShapeFactory();
-        // get an object of Shape Rectangle
-        Rectangle rectangle = new Rectangle();
-        // call draw method of Shape Rectangle
-        rectangle.draw();
-        // get an object of Shape Square
-        Square square = new Square();
-        // call draw method of Shape Square
-        square.draw();
-        // get rounded shape factory
-        RoundedShapeFactory factory1 = new RoundedShapeFactory();
-        // get an object of Rounded Shape Rectangle
-        RoundedRectangle rectangle1 = new RoundedRectangle();
-        // call draw method of Rounded Shape Rectangle
-        rectangle1.draw();
-        // get an object of Rounded Shape Square
-        RoundedSquare square1 = new RoundedSquare();
-        // call draw method of Rounded Shape Square
-        square1.draw();
-    }
+	// TO-DO: Implement the main() method for testing purpose
+	public static void main(String[] args) {
+		// get shape factory
+		AbstractFactory shapeFactory = FactoryProducer.getFactory(false);
+		// get an object of Shape Rectangle
+		Shape shape1 = shapeFactory.getShape("RECTANGLE");
+		// call draw method of Shape Rectangle
+		shape1.draw();
+		// get an object of Shape Square
+		Shape shape2 = shapeFactory.getShape("SQUARE");
+		// call draw method of Shape Square
+		shape2.draw();
+		// get shape factory
+		AbstractFactory shapeFactory1 = FactoryProducer.getFactory(true);
+		// get an object of Shape Rectangle
+		Shape shape3 = shapeFactory1.getShape("RECTANGLE");
+		// call draw method of Shape Rectangle
+		shape3.draw();
+		// get an object of Shape Square
+		Shape shape4 = shapeFactory1.getShape("SQUARE");
+		// call draw method of Shape Square
+		shape4.draw();
+
+	}
 }
